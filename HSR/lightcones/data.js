b15ea14348f9b091ca3f57b6ca3532be704ecaa4
AlbaVika <jcsbernal@hotmail.com>
2024-09-13 13:00:31 +0200

    Create data.js

---
+++ HSR/lightcones/data.js
@@ -0,0 +1,8 @@
+lcdata={
+	"ASecretVow":{"name":"A Secret Vow","path":"Destruction","rarity":4,"stats":[10,9,4],"ddata":[[20,25,30,35,40],[20,25,30,35,40]],"description":["Increases DMG dealt by the wearer by <b style='color:blue'>","%</b>. The wearer also deals an extra <b style='color:blue'>","%</b> of DMG to enemies whose current HP percentage is equal to or higher than the wearer's current HP percentage."]},
+	"Adversarial":{"name":"Adversarial","path":"Hunt","rarity":3,"stats":[7,7,4],"ddata":[[10,12,14,16,18]],"description":["When the wearer defeats an enemy, increases SPD by <b style='color:blue'>","%</b> for 2 turn(s)"]},
+	"AfterTheCharmonyFall":{"name":"After the Charmony Fall","path":"Erudition","rarity":4,"stats":[8,9,6],"ddata":[[28,35,32,49,56],[8,10,12,14,16]],"description":["Increases the wearer's Break Effect by <b style='color:blue'>","%</b>. After the wearer uses Ultimate, increases SPD by <b style='color:blue'>","%</b>, lasting for 2 turn(s)."]},
+	"AlongThePassingShore":{"name":"Along The Passing Shore","path":"Nihility","rarity":5},
+	"Amber":{"name":"Amber","path":"Preservation","rarity":3},
+	"AnInstantBeforeAGaze":{"name":"An Instant Before A Gaze","path":"Erudition","rarity":5}
+};
